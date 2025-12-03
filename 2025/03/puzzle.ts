e6cb8b6b95c36bc5c1cdcf5340871cd1038c5494
Pascal Mathys <pascal.mathys@rootix.ch>
2025-12-03 07:28:58 +0100

    2025 - Day 03

---
+++ 2025/03/puzzle.ts
@@ -0,0 +1,38 @@
+﻿import { getInput, execute, splitLinesIntoArray } from '../utils';
+
+const part1 = (): number => {
+    return calculateMaxPower(2);
+};
+
+const part2 = (): number => {
+    return calculateMaxPower(12);
+};
+
+const calculateMaxPower = (numberOfBatteriesToTurnOn: number): number => {
+    const banks = splitLinesIntoArray(getInput()).map((line) => line.split('').map(Number));
+    let power = 0;
+
+    for (const bank of banks) {
+        const relevantBatteries = [...bank];
+        while (relevantBatteries.length > numberOfBatteriesToTurnOn) {
+            let removedBattery = false;
+            for (let i = 0; i < relevantBatteries.length - 1; i++) {
+                if (relevantBatteries[i] < relevantBatteries[i + 1]) {
+                    relevantBatteries.splice(i, 1);
+                    removedBattery = true;
+                    break;
+                }
+            }
+
+            if (!removedBattery) {
+                relevantBatteries.splice(relevantBatteries.length - 1, 1);
+            }
+        }
+
+        power += Number(relevantBatteries.reduce((acc, curr) => acc + curr.toString(), ''));
+    }
+
+    return power;
+};
+
+execute([part1, part2]);
--- README.md
+++ README.md
@@ -5,4 +5,4 @@ These are my solutions to all Advent of Code puzzles i've solved (I can now fina
 - [2022](./2022) - In TypeScript
 - [2023](./2023) - In C#
 - [2024](./2024) - In Python (incomplete)
-- [2023](./2023) - In Typescript
+- [2025](./2025) - In Typescript
